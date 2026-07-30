d6a2427c6afe6a269946c823feeb5b20b8c52bba
shagunujjwal <shagunujjwal4@gmail.com>
2026-07-30 18:03:08 +0530

    fix App.jsx and signup import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
 import Login from "./pages/Login";
-import Signup from "./pages/Signup";
+import Signup from "./pages/signup";
+
 import Dashboard from "./pages/Dashboard";
 import Customers from "./pages/Customers";
 import Agents from "./pages/Agents";
@@ -13,94 +13,38 @@ import Documents from "./pages/Documents";
 import Settings from "./pages/Settings";
 
 
+function App(){
 
-function App() {
-
-
-  return (
-
+  return(
     <BrowserRouter>
-
 
       <Routes>
 
+        <Route path="/" element={<Login />} />
 
-        <Route
-          path="/"
-          element={<Login />}
-        />
+        <Route path="/signup" element={<Signup />} />
 
+        <Route path="/dashboard" element={<Dashboard />} />
 
-        <Route
-          path="/signup"
-          element={<Signup />}
-        />
+        <Route path="/customers" element={<Customers />} />
 
+        <Route path="/agents" element={<Agents />} />
 
+        <Route path="/policies" element={<Policies />} />
 
-        <Route
-          path="/dashboard"
-          element={<Dashboard />}
-        />
+        <Route path="/claims" element={<Claims />} />
 
+        <Route path="/payments" element={<Payments />} />
 
+        <Route path="/documents" element={<Documents />} />
 
-        <Route
-          path="/customers"
-          element={<Customers />}
-        />
-
-
-
-        <Route
-          path="/agents"
-          element={<Agents />}
-        />
-
-
-
-        <Route
-          path="/policies"
-          element={<Policies />}
-        />
-
-
-
-        <Route
-          path="/claims"
-          element={<Claims />}
-        />
-
-
-
-        <Route
-          path="/payments"
-          element={<Payments />}
-        />
-
-
-
-        <Route
-          path="/documents"
-          element={<Documents />}
-        />
-
-
-
-        <Route
-          path="/settings"
-          element={<Settings />}
-        />
-
+        <Route path="/settings" element={<Settings />} />
 
       </Routes>
 
-
     </BrowserRouter>
-
   );
 
 }
-
 
 export default App;
